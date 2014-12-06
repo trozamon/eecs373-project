19db2912810ddda3999a45684978de4f3751f484
Chris Oo <coo@umich.edu>
2014-12-06 16:27:25 -0500

    add ADC

--- ble_app_beacon/main.c
+++ ble_app_beacon/main.c
@@ -19,12 +19,15 @@
 #include "nrf.h"
 #include "nrf_gpiote.h"
 #include "app_util_platform.h"
+#include "app_timer.h"
 #include "nrf_delay.h"
+
 
 #define LED_0 16
 
 #define Pin_FromSoil 2
 #define Pin_ToSoil 0
+#define PIN_TEMP_ENABLE 6
 
 #define DEVICE_NAME "SS"
 
@@ -43,6 +46,10 @@
 #define APP_COMPANY_IDENTIFIER           0xEFBE                           /**< Company identifier for Apple Inc. as per www.bluetooth.org. */
 #define APP_MAJOR_VALUE                  0x01, 0x02                        /**< Major value used to identify Beacons. */ 
 #define APP_MINOR_VALUE                  0x03, 0x04                        /**< Minor value used to identify Beacons. */ 
+
+#define ADC_SAMPLING_INTERVAL                APP_TIMER_TICKS(100, APP_TIMER_PRESCALER) /**< Sampling rate for the ADC */
+
+static app_timer_id_t                        m_adc_sampling_timer_id;      /**< ADC timer */
 
 // Hijack the UUID to send data, along with the MAJOR and MINOR values.
 #define APP_BEACON_UUID                  0xDE, 0xAD, 0xBE, 0xEF, \
@@ -70,6 +77,7 @@ static uint8_t m_beacon_info[APP_BEACON_INFO_LENGTH] =                     /**< 
 };
 
 static uint32_t TIME_DELAY = 0xFFFFFFFF;
+static uint32_t TEMPERATURE = 0xFFFFFFFF;
 
 
 /**@brief Function for error handling, which is called when an error has occurred.
@@ -125,6 +133,7 @@ static void gpio_init(void)
     // Init soil probes
     nrf_gpio_cfg_input(Pin_FromSoil, NRF_GPIO_PIN_NOPULL);
     nrf_gpio_cfg_output(Pin_ToSoil);
+		nrf_gpio_cfg_output(PIN_TEMP_ENABLE);
 
     // Init advertising LED
     nrf_gpio_cfg_output(ADVERTISING_LED_PIN_NO);
@@ -149,7 +158,7 @@ void timers_init(void)
 void wait_to_zero(volatile NRF_TIMER_Type* p_timer, int timeToWait)
 {
 	//Waits for falling edge to completely zero out
-	nrf_gpio_pin_write(Pin_ToSoil, 0);
+	nrf_gpio_pin_clear(Pin_ToSoil);
 	p_timer->TASKS_CLEAR = 1;
 	p_timer->TASKS_START = 1;
 	p_timer->CC[0] = timeToWait; 
@@ -162,10 +171,10 @@ uint32_t find_time_delay(volatile NRF_TIMER_Type* p_timer, int numSamples)
 {
     unsigned int difference[100];
     unsigned char i = 0;
-    wait_to_zero(NRF_TIMER1, 750);
+    wait_to_zero(NRF_TIMER1, 2000);
     while (i < numSamples)
     {
-        nrf_gpio_pin_write(Pin_ToSoil, 1); //Send pulse
+        nrf_gpio_pin_set(Pin_ToSoil); //Send pulse
         p_timer->TASKS_CAPTURE[0] = 1; //Start timer
         while (!nrf_gpio_pin_read(Pin_FromSoil)) {} //Wait for return pulse
         p_timer->TASKS_CAPTURE[1] = 1; //Stop timer
@@ -174,10 +183,10 @@ uint32_t find_time_delay(volatile NRF_TIMER_Type* p_timer, int numSamples)
         if (currDifference < 0xFFFF && currDifference > 0x0000) //Make sure delay is valid
         {
             difference[i++] = currDifference;
-            wait_to_zero(NRF_TIMER1, 750);
+            wait_to_zero(NRF_TIMER1, 3 * currDifference);
         }
         else
-            wait_to_zero(NRF_TIMER1, 750);
+            wait_to_zero(NRF_TIMER1, 2000);
     }
     
     //Average pulse duration
@@ -224,6 +233,17 @@ static void advertising_init(void)
     uint8_t index = MAJ_VAL_OFFSET_IN_BEACON_INFO;
 
     m_beacon_info[index++] = MSB(major_value);
+    m_beacon_info[index++] = LSB(major_value);
+
+    m_beacon_info[index++] = MSB(minor_value);
+    m_beacon_info[index++] = LSB(minor_value);
+		
+		major_value = (TEMPERATURE & 0xFFFF0000) >> 16;
+		minor_value = (TEMPERATURE & 0x0000FFFF);
+		
+		index = MAJ_VAL_OFFSET_IN_BEACON_INFO - 4;
+		
+		m_beacon_info[index++] = MSB(major_value);
     m_beacon_info[index++] = LSB(major_value);
 
     m_beacon_info[index++] = MSB(minor_value);
@@ -336,6 +356,70 @@ static void power_manage(void)
     APP_ERROR_CHECK(err_code);
 }
 
+// ADC timer handler to start ADC sampling
+static void adc_sampling_timeout_handler(void)
+{
+    uint32_t p_is_running = 0;
+        
+    sd_clock_hfclk_request();
+    while(! p_is_running) {                             //wait for the hfclk to be available
+        sd_clock_hfclk_is_running((&p_is_running));
+    }               
+    NRF_ADC->TASKS_START = 1;                           //Start ADC sampling
+}
+
+static void adc_init(void)
+{   
+		// Enable temp sensor
+		nrf_gpio_pin_set(PIN_TEMP_ENABLE);
+	
+    /* Enable interrupt on ADC sample ready event*/     
+    NRF_ADC->INTENSET = ADC_INTENSET_END_Msk;   
+    sd_nvic_SetPriority(ADC_IRQn, NRF_APP_PRIORITY_LOW);  
+    sd_nvic_EnableIRQ(ADC_IRQn);
+    
+    NRF_ADC->CONFIG = (ADC_CONFIG_EXTREFSEL_None << ADC_CONFIG_EXTREFSEL_Pos) /* Bits 17..16 : ADC external reference pin selection. */
+                                    | (ADC_CONFIG_PSEL_AnalogInput2 << ADC_CONFIG_PSEL_Pos)                 /*!< Use analog input 2 as analog input. */
+                                    | (ADC_CONFIG_REFSEL_VBG << ADC_CONFIG_REFSEL_Pos)                          /*!< Use internal 1.2V bandgap voltage as reference for conversion. */
+                                    | (ADC_CONFIG_INPSEL_AnalogInputOneThirdPrescaling << ADC_CONFIG_INPSEL_Pos) /*!< Analog input specified by PSEL with no prescaling used as input for the conversion. */
+                                    | (ADC_CONFIG_RES_8bit << ADC_CONFIG_RES_Pos);                                  /*!< 8bit ADC resolution. */ 
+    
+    /* Enable ADC*/
+    NRF_ADC->ENABLE = ADC_ENABLE_ENABLE_Enabled;
+}
+
+/* Interrupt handler for ADC data ready event */
+void ADC_IRQHandler(void)
+{
+    /* Clear dataready event */
+  NRF_ADC->EVENTS_END = 0;  
+
+  /* Write ADC result to tempurature */
+	TEMPERATURE = NRF_ADC->RESULT;
+	
+	sd_nvic_DisableIRQ(ADC_IRQn);
+	nrf_gpio_pin_clear(PIN_TEMP_ENABLE);
+    
+    //Use the STOP task to save current. Workaround for PAN_028 rev1.5 anomaly 1.
+  NRF_ADC->TASKS_STOP = 1;
+    
+    //Release the external crystal
+    sd_clock_hfclk_release();
+	
+		NRF_ADC->ENABLE = ADC_ENABLE_ENABLE_Disabled;
+	
+	// Start advertisment.
+		advertising_init();
+    advertising_start();
+
+    // Only keep LED on for 200000 us
+    nrf_delay_us(200000);
+    nrf_gpio_pin_clear(ADVERTISING_LED_PIN_NO);
+		
+		nrf_delay_us(1000000);
+		NVIC_SystemReset();
+}  
+
 /**
  * @brief Function for application main entry.
  */
@@ -349,6 +433,7 @@ int main(void)
     // Find sensor data
 		uint32_t lastSample = 0;
 		uint32_t currSample = 0;
+		const uint32_t epsilon = 15;
 		do {
 				lastSample = currSample;
 				currSample = find_time_delay(NRF_TIMER0, 100);
@@ -357,14 +442,11 @@ int main(void)
 
     // Init BLE
     ble_stack_init();
-    advertising_init();
-
-    // Start execution.
-    advertising_start();
-
-    // Only keep LED on for 200000 us
-    nrf_delay_us(200000);
-    nrf_gpio_pin_clear(ADVERTISING_LED_PIN_NO);
+		
+		// Read temperature
+		adc_init();
+		adc_sampling_timeout_handler();
+		
 
     // Enter main loop.
     for (;;)
