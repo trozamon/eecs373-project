16172ec7577552e312b32b38ea39ac95ffeb4d1b
Alec Ten Harmsel <talec@umich.edu>
2014-12-07 15:39:20 -0500

    Moisture plot is how human-readable

--- py/plot.py
+++ py/plot.py
@@ -1,35 +1,67 @@
-from matplotlib.pyplot import plot, figure, close, title, subplot
+from matplotlib.pyplot import plot, figure, close, title, subplot, hold
 from matplotlib.backends.backend_pdf import PdfPages
 from time import sleep
 
 while True:
     print('Plotting...')
 
-    x = []
+    x = 1
+
+    humid_ts = []
     humids = []
+    temp_ts = []
     temps = []
+    humid = 0
+    temp = 0
     with open('output.txt') as f:
         for line in f:
             try:
-                humid = int(line.split(',')[1])
+                # Do str -> int conversions first; ensure both succeed before
+                # committing to data
+                humid = min(2000, float(line.split(',')[1]))
                 temp = int(line.split(',')[2])
+
+                # Values less than 400 mean a sensor that's not in soil, or in
+                # soil so dry it can not get a measurement
+                if humid >= 400.0:
+                    humid = humid - 400.0
+                    humid = humid / 1600.0
+                    humid = 1 - humid
+                    humid = humid * 100
+                    humids.append(humid)
+                    humid_ts.append(x)
+
                 temps.append(temp)
-                humids.append(humid)
-                x.append(len(x) + 1)
+                temp_ts.append(x)
+                x = x + 1
             except ValueError:
                 pass
 
     with PdfPages('humidity.pdf') as pdf:
         figure()
-        plot(x, humids)
-        title(' '.join(['Current Humidity:', str(humids[-1])]))
+
+        start = 1
+        stop = 1
+        while start < humid_ts[-1]:
+            stop = start + 1
+            while stop < len(humid_ts) and (stop - start) == (humid_ts[stop] - humid_ts[start]):
+                stop = stop + 1
+            plot(humid_ts[start:stop], humids[start:stop], 'b.-')
+            hold(True)
+            start = stop
+
+        if humid >= 400:
+            title(' '.join(['Current Moisture Level:', str(humids[-1])]))
+        else:
+            title('Current Moisture Level: Not in soil')
+
         pdf.savefig()
         close()
 
     with PdfPages('temperature.pdf') as pdf:
         figure()
-        plot(x, temps)
-        title(' '.join(['Current Temperature:', str(temps[-1])]))
+        plot(temp_ts, temps)
+        title(' '.join(['Current Temperature:', str(temp)]))
         pdf.savefig()
         close()
 
